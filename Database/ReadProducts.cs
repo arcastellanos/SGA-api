2aa616c40ab9c5541098cf489111add9551523aa
aabarron1 <aabarron@crimson.ua.edu>
2023-04-04 14:25:56 -0500

    AB/workingOnApi

--- Database/ReadProducts.cs
+++ Database/ReadProducts.cs
@@ -19,21 +19,16 @@ namespace SGA_api.Database
 
             con.Open();
 
-            string stm = "SELECT * FROM d82kvyquj6n9y0g6";
-                using var cmd = new MySqlCommand(stm, con);
+            string stm = "SELECT * FROM d82kvyquj6n9y0g6.PRODUCT";
+            using var cmd = new MySqlCommand(stm, con);
 
-                using MySqlDataReader rdr = cmd.ExecuteReader();
+            using MySqlDataReader rdr = cmd.ExecuteReader();
 
-                while (rdr.Read())
-                {
-                Product temp = new Product()
-                    {
-
-
-                    };
-                    allProducts.Add(temp);
-                }
-                return allProducts;
+            while (rdr.Read()) {
+                Product temp = new Product(){ProductID = rdr.GetInt32(0), ProductName = rdr.GetString(1), ProductCategory = rdr.GetInt32(2), ProductPrice = rdr.GetInt32(3), ManagerID = rdr.GetInt32(4)};
+                allProducts.Add(temp);
+            }
+            return allProducts;
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,6 +2,10 @@ namespace SGA_api.Models
 {
     public class Product
     {
-        
+        public int ProductID {get; set;}
+        public string ProductName {get; set;}
+        public int ProductCategory {get; set;}
+        public int ProductPrice {get; set;}
+        public int ManagerID {get; set;}
     }
 }
